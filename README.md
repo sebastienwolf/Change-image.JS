a790185b40ad63925493e236a12853c47877c0cc
Sébastien Mouret <s.mouret.pro@gmail.com>
2022-01-26 15:53:31 +0100

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# Change-image.JS
+
+Entrainement, le but est de changer une image en cliquant sur des boutons.
---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("koala").addEventListener("click", function(){
+    document.getElementById("animals").src="images/koala.jpg"
+    });
+    
+    document.getElementById("panda").addEventListener("click", function(){
+    document.getElementById("animals").src="images/panda.jpg"
+    });
+    
+    document.getElementById("pengu").addEventListener("click", function(){
+    document.getElementById("animals").src="images/penguin.jpg"
+    });
